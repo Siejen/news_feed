6a1fbc411bfbf2fdd4a4ed6736f23cbb638696c8
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-07-21 19:47:16 -0700

    working version of article app

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var articlesArray = [];
+
+app.use(bodyParser.urlencoded());
+
 app.set('view engine', 'ejs');
 
 //homepage
@@ -9,29 +13,38 @@ app.get('/', function(req, res){
   res.render('site/index');
 });
 
-//displays summary
+//displays summary and prints all articles
 app.get('/articles', function(req, res){
-  res.send('Summary');
+  	res.render('articles/article', {articles: articlesArray});
 });
 
-//gets a form to save to article
+//form to create new article
 app.get('/articles/new', function(req, res){
-	res.send('save a new article');
+	res.render('articles/new');
 });
 
-//find the article in an array
+//find the article in an array at a specific index
 app.get('/articles/:id', function(req, res){
-	res.send('find an article in array');
+	var index = req.params.id;
+	var taco = articlesArray[index];
+	res.render('articles/show', {article: taco});
+});
+
+app.post('/articles', function(req, res){
+	console.log(req.body.articles);
+	articlesArray.push(req.body.articles);
+	console.log(articlesArray);
+	res.redirect('/articles');
 });
 
 //static page about the daily planet
 app.get('/about', function(req, res){
-	res.send('about page');
+	res.render('site/about');
 });
 
 //contact
 app.get('/contact', function(req, res){
-	res.send('contact page');
+	res.render('site/contact');
 });
 
 app.listen(3000);
